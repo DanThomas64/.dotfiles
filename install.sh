f886d6b3aa10d16e44300e5e092bac492cb4f028
DanThomas64 <danstevethomas@gmail.com>
2022-09-10 18:35:39 +0100

    install

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOTFILES="$HOME/.dotfiles"
+URL="https://github.com/DanThomas64/.dotfiles/"
+
+dotfiles () {
+  git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" "$@"
+}
+
+git clone -b linux --bare $URL $DOTFILES
+dotfiles config --local status.showUntrackedFiles no
+dotfiles checkout || echo -e 'Deal with conflicting files, then run (possibly with -f flag if you are OK with overwriting)\ndtf checkout'
+
+# Also should be used to install ZSH and OH-MY-ZSH exa neovim

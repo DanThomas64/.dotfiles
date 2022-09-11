67343fefc769f9d15beef528981495b5c7a17713
DanThomas64 <danstevethomas@gmail.com>
2022-09-11 03:38:13 +0100

    mmore more more

--- install.sh
+++ install.sh
@@ -12,3 +12,4 @@ dotfiles config --local status.showUntrackedFiles no
 dotfiles checkout || echo -e 'Deal with conflicting files, then run (possibly with -f flag if you are OK with overwriting)\ndtf checkout'
 
 # Also should be used to install ZSH and OH-MY-ZSH exa neovim
+# nodejs, perl, rust, 

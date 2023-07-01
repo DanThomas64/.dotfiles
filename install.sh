92417036494ce6005b14e44771aa8e68296c3093
Dan Thomas [BadWolf] <5899996+DanThomas64@users.noreply.github.com>
2023-07-01 19:46:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,3 +13,43 @@ dotfiles checkout || echo -e 'Deal with conflicting files, then run (possibly wi
 
 # Also should be used to install ZSH and OH-MY-ZSH exa neovim
 # nodejs, perl, rust, 
+
+declare -a fonts=(
+	Hack
+)
+
+version='2.2.2'
+fonts_dir="${HOME}/.local/share/fonts"
+
+if [[ ! -d "$fonts_dir" ]]; then
+    mkdir -p "$fonts_dir"
+fi
+
+for font in "${fonts[@]}"; do
+    zip_file="${font}.zip"
+    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
+    echo "Downloading $download_url"
+    wget "$download_url"
+    unzip "$zip_file" -d "$fonts_dir" -x "*.txt/*" -x "*.md/*"
+    rm "$zip_file"
+done
+
+find "$fonts_dir" -name '*Windows Compatible*' -delete
+
+fc-cache -fv
+
+declare -a apps =(
+	nvim
+	fzf
+	treesitter
+	zsh
+	ohmyzsh
+	era
+	bat
+)
+
+for app in "${apps[@]}"; do
+    apt install -y ${app[@]}
+done
+
+sudo chsh -s /usr/bin/zsh $USERNAME

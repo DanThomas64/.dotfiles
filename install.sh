cce9200e0772be05329d16cf8cda107c0b75af22
Dan Thomas [BadWolf] <5899996+DanThomas64@users.noreply.github.com>
2023-07-07 01:29:31 -0300

    Latest bs that I can't remember

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 DOTFILES="$HOME/.dotfiles"
 URL="https://github.com/DanThomas64/.dotfiles/"
 
-dotfiles () {
-  git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" "$@"
+dotfiles() {
+	git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" "$@"
 }
 
 git clone -b linux --bare $URL $DOTFILES
@@ -12,7 +12,7 @@ dotfiles config --local status.showUntrackedFiles no
 dotfiles checkout || echo -e 'Deal with conflicting files, then run (possibly with -f flag if you are OK with overwriting)\ndtf checkout'
 
 # Also should be used to install ZSH and OH-MY-ZSH exa neovim
-# nodejs, perl, rust, 
+# nodejs, perl, rust,
 
 declare -a fonts=(
 	Hack
@@ -22,34 +22,34 @@ version='2.2.2'
 fonts_dir="${HOME}/.local/share/fonts"
 
 if [[ ! -d "$fonts_dir" ]]; then
-    mkdir -p "$fonts_dir"
+	mkdir -p "$fonts_dir"
 fi
 
 for font in "${fonts[@]}"; do
-    zip_file="${font}.zip"
-    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
-    echo "Downloading $download_url"
-    wget "$download_url"
-    unzip "$zip_file" -d "$fonts_dir" -x "*.txt/*" -x "*.md/*"
-    rm "$zip_file"
+	zip_file="${font}.zip"
+	download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
+	echo "Downloading $download_url"
+	wget "$download_url"
+	unzip "$zip_file" -d "$fonts_dir" -x "*.txt/*" -x "*.md/*"
+	rm "$zip_file"
 done
 
 find "$fonts_dir" -name '*Windows Compatible*' -delete
 
 fc-cache -fv
 
-declare -a apps =(
+declare -a apps=(
 	nvim
 	fzf
 	treesitter
 	zsh
 	ohmyzsh
-	era
+	exa
 	bat
 )
 
 for app in "${apps[@]}"; do
-    apt install -y ${app[@]}
+	apt install -y ${app[@]}
 done
 
 sudo chsh -s /usr/bin/zsh $USERNAME

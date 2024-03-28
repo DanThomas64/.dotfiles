a7c5f6bad401f4093c71bad6d79caa0c81647b5e
Dan Thomas <5899996+DanThomas64@users.noreply.github.com>
2024-03-28 12:19:25 -0300

    comments and nvim  deploy

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-DOTFILES="$HOME/.dotfiles"
-URL="https://github.com/DanThomas64/.dotfiles/"
-
+# Install required apps
 declare -a apps=(
 	neovim
 	fzf
@@ -11,25 +9,35 @@ declare -a apps=(
 	exa
 	bat
 	stow
+	github-cli
 )
 
 for app in "${apps[@]}"; do
 	yay -S ${app[@]} --noconfirm
 done
 
+# Download and deploy dotfile git repo.
+DOTFILES="$HOME/.dotfiles"
+URL="https://github.com/DanThomas64/.dotfiles/"
 git clone $URL $DOTFILES
-
 cd $DOTFILES
-
 stow . --adopt
-
 git restore .
-
 cd $HOME
 
-# Also should be used to install ZSH and OH-MY-ZSH exa neovim
-# nodejs, perl, rust,
+# Auth GitHub for further setup
+gh auth login -w
 
+# Download and deploy nvim config
+VIMRC="$HOME/.config/nvim"
+
+if [[ ! -d "$VIMRC" ]]; then
+	cp "$VIMRC" "${VIMRC}_backup"
+fi
+
+gh repo clone "DanThomas64/nvim" $VIMRC
+
+# Install hack Font
 declare -a fonts=(
 	Hack
 )
@@ -55,6 +63,6 @@ find "$fonts_dir" -name '*Windows Compatible*' -delete
 fc-cache -fv
 fc-match "Hack Nerd Font Mono" -a
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+# Install oh-my-zsh and set zsh as default
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --keep-zshrc
 sudo chsh -s /usr/bin/zsh $USERNAME
